6e9ff4e0a7d2600b89b7ba545689117f80aeffc5
tommcintyre <tommcintyre@montvieux.com>
2014-05-09 13:43:09 +0100

    Add script to install required dependencies on Ubuntu

--- BDE_do_config.sh
+++ BDE_do_config.sh
@@ -4,7 +4,7 @@
 		--disable-shared --enable-static --disable-debug --disable-doc --enable-runtime-cpudetect --disable-ffserver \
 		--enable-gpl --enable-version3 --enable-nonfree \
 		--enable-bzlib --enable-zlib \
-		--enable-libmp3lame --enable-libx264 --enable-libwebp --enable-libspeex --enable-libvorbis \
+		--enable-libmp3lame --enable-libx264 --enable-libspeex --enable-libvorbis \
 		--enable-libvpx --enable-libxvid --enable-libtheora --enable-libvo-aacenc --enable-libvo-amrwbenc \
-		--enable-gray --enable-libopenjpeg --enable-libopus
+		--enable-gray --enable-libopenjpeg
 
