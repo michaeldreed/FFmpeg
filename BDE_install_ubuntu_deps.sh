b912758ba245f5a4bb3262094bb2c83e97f5393c
tommcintyre <tommcintyre@montvieux.com>
2014-05-10 12:02:21 +0100

    Add PNG deps

--- BDE_install_ubuntu_deps.sh
+++ BDE_install_ubuntu_deps.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-sudo apt-get install build-essential yasm pkg-config ant-optional junit libmp3lame-dev libopenjpeg-dev libogg-dev libspeex-dev libtheora-dev libvo-aacenc-dev libvo-amrwbenc-dev libvorbis-dev libvpx-dev libx264-dev libxvidcore-dev
+sudo apt-get install build-essential yasm pkg-config ant-optional junit libmp3lame-dev libopenjpeg-dev libogg-dev libspeex-dev libtheora-dev libvo-aacenc-dev libvo-amrwbenc-dev libvorbis-dev libvpx-dev libx264-dev libxvidcore-dev libpng12-dev zlib1g-dev
 
